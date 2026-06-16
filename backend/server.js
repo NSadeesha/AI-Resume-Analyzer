8493aa1bc58ab4d8b899e91bba929d4361f2b7ae
Sadeesha <nethmisadeesha2002@gmail.com>
2026-06-16 09:07:12 +0530

    "MongoDB Atlas connection setup"
    "MongoDB Atlas connection setup"

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./src/config/db");
+
+// Connect Database
+connectDB();
 
 const app = express();
 
