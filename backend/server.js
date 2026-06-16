237954832de57bc21ad48651b58c7bea7c28b7e4
Sadeesha <nethmisadeesha2002@gmail.com>
2026-06-16 09:23:43 +0530

    Create user model and authentication routes
    Create user model and authentication routes

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,23 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./src/config/db");
+const userRoutes = require("./src/routes/userRoutes");
 
 // Connect Database
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/users", userRoutes);
+
 app.get("/", (req, res) => {
   res.json({
-    message: "AI Resume Analyzer API Running"
+    message: "AI Resume Analyzer API Running",
   });
 });
 
---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,7 @@
+exports.registerUser = async (req, res) => {
+  res.json({ message: "Register User" });
+};
+
+exports.loginUser = async (req, res) => {
+  res.json({ message: "Login User" });
+};
